9a8c8826751ddfb5eba15754bf27e34d260ac319
root <pcabreram@edenorte.com.do>
2025-03-20 21:27:19 -0400

    Corregido el margen desproporcional entre el logo del aplicativo y el formulario de login

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -38,6 +38,7 @@ const LogInForm = () => {
           alignItems: "center",
           justifyContent: "center",
           textAlign: "center",
+          alignContent: "center",
         }}
       >
         <Image
